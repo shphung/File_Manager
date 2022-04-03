af771eb7c025cb226d3260c4cb95bbdd16fae2a3
shphung <stevenphunghuynh@yahoo.com>
2022-04-02 20:51:51 -0700

    Initiated basic project files

---
+++ FileManager/src/filemanager/FileManager.java
@@ -0,0 +1,19 @@
+/**
+ * File: FileManager.java
+ * Authors: Steven Phung, 
+ * Class: CECS-544-01 - Software Test and Verification
+ * 
+ * Assignment: Semester Project
+ * Due Date: April 20, 2022 @ 5pm
+ * 
+ * Purpose: This is the main class that will call and run other classes.
+ */
+package filemanager;
+
+public class FileManager {
+
+    public static void main(String[] args) {
+        //new MainFrame();
+    }
+    
+}
